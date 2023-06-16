2cc8df9ae1fa5aae9edecb7add1d6eef77889fa1
TheAdedejibrain <aoluwagbenro1@gmail.com>
2023-06-16 08:39:20 -0700

    4.Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
   *main - this is the main function
   *
-  *Return : always 0
+  *Return: always 0
   */
-int main (void)
+
+int main(void)
 {
 	puts("\"programming is like building a multilingual puzzle");
 	return (0);
