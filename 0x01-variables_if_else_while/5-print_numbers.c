5427bfb6f5c9fe8e58165f47c19461e87e1ef49d
TheAdedejibrain <aoluwagbenro1@gmail.com>
2023-06-18 04:46:18 -0700

    5.Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+  * main - print all numbers from 0 to 10
+  * Return: always 0 (success)
+  */
+
+int main(void)
+
+{
+	int i;
+		for (i = 0; i < 10; i++)
+			printf("%d", i);
+	printf("\n");
+	return (0);
+}
