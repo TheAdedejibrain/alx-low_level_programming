8ea4b6ed362d2b32012ebf8c2bc1220ab7b24faf
TheAdedejibrain <aoluwagbenro1@gmail.com>
2023-06-18 14:17:37 -0700

    8.print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  * main - prints base 16 numbers in lowercase
+  * Return: alwats 0 (success)
+  */
+
+int main(void)
+
+{
+	char x;
+
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	for (x = 'a'; x <= 'f'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
