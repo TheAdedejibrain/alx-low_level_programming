bc8c81dc63ea4215cb1be535c8e9225e90c71c0e
TheAdedejibrain <aoluwagbenro1@gmail.com>
2023-06-19 13:35:02 -0700

    100.print comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,27 @@
-#include <stdio.h>
+#include<stdio.h>
 /**
-  * main - prints all possible different combinations of two digits
-  * Return: always 0 (success)
-  */
-
+ *main - prints numbers between 00 to 89.
+ *
+ *Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	int i;
-
-	for (i = 10; i < 100; i++)
-
-	putchar(i);
-
-	{
-		putchar(',');
-		putchar(' ');
-	}
-	return (0);
+int i, j;
+for (i = 48; i <= 56; i++)
+{
+for (j = 49; j <= 57; j++)
+{
+if (i < j)
+{
+putchar(i);
+putchar(j);
+if (i == 56 && j == 57)
+continue;
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
 }
